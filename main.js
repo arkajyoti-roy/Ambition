d4503122b324608594265dd2818354a6ed2f44c0
arka445 <163657852+arka445@users.noreply.github.com>
2024-05-21 22:19:48 +0530

    ip

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
+    loop: true,
     autoplay: {
       delay: 2000,
       disableOnInteraction: false,
@@ -12,5 +13,22 @@ var swiper = new Swiper(".mySwiper", {
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+
+  var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
     },
   });
