8d4583e0e4e73bef7186301e32cbebf7df5356a0
arka445 <163657852+arka445@users.noreply.github.com>
2024-05-22 22:25:55 +0530

    Updates

--- main.js
+++ main.js
@@ -16,19 +16,3 @@ var swiper = new Swiper(".mySwiper", {
     },
   });
 
-
-
-  var swiper = new Swiper(".mySwiper2", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    freeMode: true,
-    loop: true,
-    autoplay: {
-      delay: 2000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
